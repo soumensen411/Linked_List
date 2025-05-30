d5b5b95d36e1087d566c93e983d297811a1a989e
Soumen sen <144879914+soumensen411@users.noreply.github.com>
2025-05-30 12:06:49 +0600

    create a folder for doubly Linked list

--- Doubly_Linked_List/Doubly_Linked_List.cpp
+++ Doubly_Linked_List/Doubly_Linked_List.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define optimize()                \
     ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
+    cin.tie(0);                   \g
     cout.tie(0);
 #define endl '\n'
 #define ll long long
